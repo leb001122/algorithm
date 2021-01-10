5645fce371ad9286ca357c3f1aeeccbcfa7db616
EunBin <EunBin@192.168.0.31>
2021-01-10 13:49:46 +0900

    왜 틀렸는데 ㅅㅣ

---
+++ BaekJoon/src/january8/Bridge.java
@@ -0,0 +1,23 @@
+package january8;
+import java.util.*;
+public class Bridge {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcaset = sc.nextInt(), n, m;
+		double bridge, div, temp;
+		for(int i=0; i<testcaset; i++) {
+			n = sc.nextInt(); // 서쪽
+			m = sc.nextInt(); // 동쪽
+			bridge = 1; 
+			div = 1;
+			temp = n;
+			for(int j=0; j<temp; j++, m--, n--) {
+				bridge *= m;
+				div *= n;
+			}	
+			bridge /= div;
+			System.out.println((int)bridge);
+		}
+	}
+}
