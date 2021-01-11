fad848cf28571ee1812f0f350f929686f9578c3c
이은빈 <lwb001122@naver.com>
2021-01-11 11:02:05 +0900

    배열(1) 연습문제 4번

---
+++ BaekJoon/src/january8/Test.java
@@ -0,0 +1,16 @@
+package january8;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a = {4, 5, 6, 7, 9};
+		int idx = Arrays.binarySearch(a, 5);
+		System.out.println(idx);
+		
+		
+	}
+
+}
