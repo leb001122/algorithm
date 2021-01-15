2c8339d92a73fda9bac84497e2b8bbcb0dff8e4d
이은빈 <lwb001122@naver.com>
2021-01-15 10:16:37 +0900

    Bronze2 1152 wordNumber

---
+++ BaekJoon/src/uu/Bronze2_WordNumber.java
@@ -0,0 +1,27 @@
+package uu;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Bronze2_WordNumber {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		String input = br.readLine();
+		input = input.trim();
+		
+		String [] wordArray;
+		wordArray = input.split(" ");
+		
+		if(wordArray[0].matches(""))
+			System.out.println("0");
+		else {
+			System.out.println(wordArray.length);
+		}
+		
+
+	}
+
+}
